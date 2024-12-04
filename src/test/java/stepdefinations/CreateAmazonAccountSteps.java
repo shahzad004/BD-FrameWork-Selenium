42d7a215c45ccf42880dd2d9c1a9b5ce22b9dfcd
Shahzad <shahzadkhan36038@gmail.com>
2024-12-04 15:16:49 -0800

    Ok

--- src/test/java/stepdefinations/CreateAmazonAccountSteps.java
+++ src/test/java/stepdefinations/CreateAmazonAccountSteps.java
@@ -1,19 +1,21 @@
 package stepdefinations;
 
 import base.BaseTest;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Assert;
+
 import pages.amazon.CreateYourAmazonAccount_PF;
 import pages.amazon.Dashboard_PF;
 import pages.amazon.SignPage_PF;
 
 public class CreateAmazonAccountSteps extends BaseTest {
 
-
+    Dashboard_PF dashboardPage;
     SignPage_PF signPagePage;
     CreateYourAmazonAccount_PF createYourAmazonAccountPage;
 
@@ -21,7 +23,9 @@ public class CreateAmazonAccountSteps extends BaseTest {
     @Before
     public void setupDriver(){
         setup();
-
+       this.createYourAmazonAccountPage=new CreateYourAmazonAccount_PF(driver);
+      this.dashboardPage =new Dashboard_PF(driver);
+      this.signPagePage=new SignPage_PF(driver);
 
 
     }
@@ -30,53 +34,51 @@ public class CreateAmazonAccountSteps extends BaseTest {
 
     @Given("I am on the amazon web page")
     public void I_am_on_the_amazon_web_page(){
-        setup();
+        Assert.assertEquals( "failed","https://www.amazon.com/" , driver.getCurrentUrl());
 
     }
 
     @When("I will click on signIn button")
     public void I_will_click_on_signIn_button(){
         System.out.println("hi ");
-        Dashboard_PF dashboardPage=new Dashboard_PF(driver);
+
         dashboardPage.clickSignIn();}
 
     @Then("I will click on Create amazon account button")
     public void I_will_click_on_Create_amazon_account_button(){
-        SignPage_PF signPagePage=new SignPage_PF(driver);
+
         signPagePage.createAccountBtn();
     }
 
     @And("I will enter username as value for your name field")
     public void I_will_enter_username_as_value_for_your_name_field(){
-        CreateYourAmazonAccount_PF createYourAmazonAccountPage=new CreateYourAmazonAccount_PF(driver);
+
         createYourAmazonAccountPage.enterName("John khan");
     }
 
     @And("I will enter phoneNumber as value for phone number field")
     public  void I_will_enter_phoneNumber_as_value_for_phone_number_field(){
-        CreateYourAmazonAccount_PF createYourAmazonAccountPage=new CreateYourAmazonAccount_PF(driver);
+
         createYourAmazonAccountPage.enteremail("254968541");
     }
 
     @And("I will enter password as a valid password value for password field")
     public void I_will_enter_password_as_a_valid_password_value_for_password_field(){
-        CreateYourAmazonAccount_PF createYourAmazonAccountPage=new CreateYourAmazonAccount_PF(driver);
+
         createYourAmazonAccountPage.enterPassword("john@123");
     }
 
     @And("I will re-enter password as a valid password value for re-enter password field")
     public void I_will_re_enter_password_as_a_valid_password_value_for_password_field(){
-        CreateYourAmazonAccount_PF createYourAmazonAccountPage=new CreateYourAmazonAccount_PF(driver);
+
         createYourAmazonAccountPage.re_enterPass("john@123");
     }
 
     @Then("I click on verify mobile number button")
     public void I_click_on_verify_mobile_number_button(){
-        CreateYourAmazonAccount_PF createYourAmazonAccountPage=new CreateYourAmazonAccount_PF(driver);
+
         createYourAmazonAccountPage.submitBtn();
     }
-
-
 
     @After
     public void setTearDown(){
--- src/test/resources/features/AmazonAccountCreation.feature
+++ src/test/resources/features/AmazonAccountCreation.feature
@@ -1,18 +1,16 @@
 Feature: Create amazon account
 
-#  Background:
-#    Given I am on the amazon web page
-#    Then I will click on signIn button
-#    And I will click on Create amazon account button
+      Background:
+        Given I am on the amazon web page
+        Then I will click on signIn button
+        And I will click on Create amazon account button
+
 
     Scenario: Successfully create account with valid phone number
-    Given I am on the amazon web page
-    When I will click on signIn button
-    Then I will click on Create amazon account button
-    And I will enter username as value for your name field
-    And I will enter phoneNumber as value for phone number field
-    And I will enter password as a valid password value for password field
-    And I will re-enter password as a valid password value for re-enter password field
-    Then I click on verify mobile number button
+        And I will enter username as value for your name field
+        And I will enter phoneNumber as value for phone number field
+        And I will enter password as a valid password value for password field
+        And I will re-enter password as a valid password value for re-enter password field
+        Then I click on verify mobile number button
 
 
